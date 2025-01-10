f4487cdc2f805c56dc0a2d4acf37221e89e1e57c
lessw2020 <lessw@etrillium.com>
2025-01-10 09:49:31 -0800

    add check_device

--- tutorials/triton/kernels/flash_attention_fwd.py
+++ tutorials/triton/kernels/flash_attention_fwd.py
@@ -15,3 +15,15 @@ BLOCK_SIZES = {
 
 NUM_STAGES = [3, 4, 7]
 NUM_WARPS = [2, 4]
+
+
+def check_device() -> bool:
+    """Check if CUDA is available, and if the current device is a SM90+ device with FP8 support"""
+    if not torch.cuda.is_available():
+        raise RuntimeError("CUDA is not available")
+        return False  # lol, I don't think this is needed
+
+    is_SM90 = torch.cuda.get_device_capability("cuda") >= (9, 0)
+    if not is_SM90:
+        print("Warning: FlashAttention with FP8 is only supported on SM90+ devices")
+    return True
