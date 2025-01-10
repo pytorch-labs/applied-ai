9f04bf4a77a319aa63a65be677231f597df8157e
lessw2020 <lessw@etrillium.com>
2025-01-10 09:46:34 -0800

    start flash_attn_fwd

--- tutorials/triton/kernels/flash_attention_fwd.py
+++ tutorials/triton/kernels/flash_attention_fwd.py
@@ -1,1 +1,17 @@
 # flash forward v2
+
+import time
+from dataclasses import dataclass
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+import torch
+import triton
+import triton.language as tl
+
+BLOCK_SIZES = {
+    "q": [64, 128],
+    "kv": [32, 64],
+}
+
+NUM_STAGES = [3, 4, 7]
+NUM_WARPS = [2, 4]
